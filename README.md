b5811e5d763d6b9624da5521613a0e7c2ca48926
Matt Gorski <matt.gorski@gmail.com>
2019-02-04 16:12:50 -0500

    Sierra Wireless AndroidRIL_V5.1.9_ARM_5.1_V1.0_bin

---
+++ README.md
@@ -0,0 +1,1 @@
+# vendor_sierra_qualcomm
---
+++ sierra-vendor.mk
@@ -0,0 +1,36 @@
+# Copyright (C) 2019 Sierra Qualcomm Wireless
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+PRODUCT_COPY_FILES += \
+    vendor/sierra/qualcomm/proprietary/bin/init.config.ip:system/bin/init.config.ip \
+    vendor/sierra/qualcomm/proprietary/bin/DownloadTool:system/bin/DownloadTool \
+    vendor/sierra/qualcomm/proprietary/bin/init.dhcpcd:system/bin/init.dhcpcd \
+    vendor/sierra/qualcomm/proprietary/bin/SierraDMLog:system/bin/SierraDMLog \
+    vendor/sierra/qualcomm/proprietary/bin/SierraFwDl7xxx:system/bin/SierraFwDl7xxx \
+    vendor/sierra/qualcomm/proprietary/bin/SierraImgMgr:system/bin/SierraImgMgr \
+    vendor/sierra/qualcomm/proprietary/bin/SierraSARTool:system/bin/SierraSARTool \
+    vendor/sierra/qualcomm/proprietary/bin/slqssdk:system/bin/slqssdk \
+    vendor/sierra/qualcomm/proprietary/bin/swifwdnld:system/bin/swifwdnld \
+    vendor/sierra/qualcomm/proprietary/bin/swihltrace:system/bin/swihltrace \
+    vendor/sierra/qualcomm/proprietary/bin/swisdk:system/bin/swisdk \
+    vendor/sierra/qualcomm/proprietary/lib/libsierraat-ril.so:system/lib/libsierraat-ril.so \
+    vendor/sierra/qualcomm/proprietary/lib/libsierrahl-ril.so:system/lib/libsierrahl-ril.so \
+    vendor/sierra/qualcomm/proprietary/lib/libsierra-ril.so:system/lib/libsierra-ril.so \
+    vendor/sierra/qualcomm/proprietary/lib/libswigpsat.so:system/lib/libswigpsat.so \
+    vendor/sierra/qualcomm/proprietary/lib/libswigpsqmi.so:system/lib/libswigpsqmi.so \
+    vendor/sierra/qualcomm/proprietary/lib/libswims.so:system/lib/libswims.so \
+    vendor/sierra/qualcomm/proprietary/lib/libswiqmiapi.so:system/lib/libswiqmiapi.so \
+    vendor/sierra/qualcomm/proprietary/lib/libswisdkapi.so:system/lib/libswisdkapi.so \
+    vendor/sierra/qualcomm/proprietary/app/com.sierra.logs/com.sierra.logs.apk:system/app/com.sierra.logs/com.sierra.logs.apk \
+    
